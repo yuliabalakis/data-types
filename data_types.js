73de5abb6b2714a770bfa48782efba27ee1fa7ec
Yulia Balakis <yuliabalakis@gmail.com>
2022-07-05 16:09:22 +0300

    add data_types.js and results.png

---
+++ data_types.js
@@ -0,0 +1,44 @@
+let a = "5";
+let b = true;
+console.log(+a + +b);
+
+let c = "4";
+let f = 5;
+console.log(+c + +f);
+
+let d = 3;
+let e = false;
+console.log(+d + +e);
+
+let aa = "5";
+let bb = true;
+console.log(aa*bb);
+
+let cc = "4";
+let ff = 5;
+console.log(cc*ff);
+
+let dd = 3;
+let ee = false;
+console.log(dd*ee);
+
+let aaa = "5";
+let bbb = true;
+console.log(aaa/bbb);
+
+let ccc = "10";
+let fff= 5;
+console.log(ccc/fff);
+
+let ddd = 3;
+let eee = true;
+console.log(ddd/eee);
+
+let value = Number("hello");
+console.log('hello =', typeof value);
+
+let value1 = String(10);
+console.log('10 =', typeof value1);
+
+let value2 = Boolean(1);
+console.log('1 =', typeof value2);
